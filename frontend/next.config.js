6f64c49dd131150fa1350ef0bce917c7a88dfa86
Maryann Mweru <mwerumaryann@gmail.com>
2025-02-22 23:58:51 +0300

    Quick fix

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -2,11 +2,11 @@
 const nextConfig = {
   output: 'export',
   images: {
-    unoptimized: true
+    unoptimized: true,
   },
   env: {
-    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000'
-  }
+    NEXT_PUBLIC_API_URL: process.env.NODE_ENV === 'production' ? '' : 'http://localhost:5000',
+  },
 };
 
-export default nextConfig;
+export default nextConfig; // Use ES6 export
